1dcdf2b24d37f5682fc6b2f4bd690c6d1a2ff8b6
ignacio12091 <ignacio.lima@anima.edu.uy>
2019-08-09 15:52:17 -0300

    server

--- index.js
+++ index.js
@@ -16,31 +16,6 @@ app.get('/prueba', function(req, res) {
     res.json({ respuesta: 'hola' });
 });
 
-// app.get('*', function(req, res, next) {
-//     console.log(req.ip);
-//     next();
-// });
-
-// app.get('/robert', function(req, res) {
-//     res.send('Hola, yo me llamo Robert');
-// });
-
-// app.get('/english', function(req, res) {
-//     res.send('Hello, my name is Robert');
-// });
-
-// app.get('/deutsch', function(req, res) {
-//     res.send('Hallo, ich heiße Robert');
-// });
-
-// app.get('/ayuwoki', function(req, res) {
-//     fs.readFile('index.html',function (err, data){
-//         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-//         res.write(data);
-//         res.end();
-//     });
-// });
-
-// app.get('*', function(req, res) {
-//     res.send('Error 800546');
-// });
+app.get('/perfil', function(req, res) {
+    res.json({ nombre: 'Cristian Mello', edad: 15 });
+});
