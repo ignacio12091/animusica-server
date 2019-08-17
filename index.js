512cf841e69f04c5b4217eff6674e547128e9896
ignacio12091 <ignacio.lima@anima.edu.uy>
2019-08-17 15:30:28 -0300

    changes

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
-let app = express();
+const { Client } = require('pg');
 
+let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+
+const client = new Client ({
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'animusica',
+    password: 'root',
+    port: 5432,
+});
+
+client.connect(() => {
+    console.log('conectado a la BD');
+})
 
 app.listen(80, function() {
     console.log("Escuchando puerto 80");
@@ -12,8 +25,14 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/prueba', function(req, res) {
-    res.json({ respuesta: 'hola' });
+app.get('/generos', function(req, res) {
+    const response = [];
+    client.query('SELECT * from genero', (err, res) => {
+        console.log(err, res)
+        response = res.rows
+        client.end()
+    })
+    res.json(response);
 });
 
 app.get('/perfil', function(req, res) {
