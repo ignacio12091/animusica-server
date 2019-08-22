3d23279aa0533ae2bc43c047139b4e174146c7c5
ilima <ignacio.lima@anima.edu.uy>
2019-08-22 12:45:41 -0300

    changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require('path');
-const { Client } = require('pg');
+/* const path = require('path');*/
+const { Pool } = require('pg');
 
 let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
-const client = new Client ({
+/* app.use(express.static(path.join(__dirname, 'build')));
+ */
+const client = new Pool ({
     user: 'postgres',
     host: '127.0.0.1',
     database: 'animusica',
@@ -13,26 +13,27 @@ const client = new Client ({
     port: 5432,
 });
 
-client.connect(() => {
-    console.log('conectado a la BD');
-})
-
 app.listen(80, function() {
     console.log("Escuchando puerto 80");
 });
 
-app.get('/', function(req, res) {
+/* app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+}); */
 
 app.get('/generos', function(req, res) {
-    const response = [];
-    client.query('SELECT * from genero', (err, res) => {
-        console.log(err, res)
-        response = res.rows
-        client.end()
-    })
-    res.json(response);
+    client.connect();
+    client.query('SELECT * from genero', (error, response) => {
+        if(!error) {
+            let generos = [];
+            response.rows.forEach(element => {
+                generos.push(element)
+            });
+            res.json(generos);
+        } else {
+            res.send("Error fetching genres");
+        }
+    });
 });
 
 app.get('/perfil', function(req, res) {
