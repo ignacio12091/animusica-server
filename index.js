79cb12f9bc98b23a2ddf16df0e2393f6834412c0
ignacio12091 <ignacio.lima@anima.edu.uy>
2019-08-27 22:24:36 -0300

    changes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 /* const path = require('path');*/
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 let app = express();
 /* app.use(express.static(path.join(__dirname, 'build')));
  */
-const client = new Client ({
+const client = new Pool ({
     user: 'postgres',
     host: '127.0.0.1',
     database: 'animusica',
@@ -27,23 +27,47 @@ app.listen(80, function() {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 }); */
 
-app.get('/canciones/:id', function(req, res) {
-    console.log('path: ', __dirname)
-    console.log(req.params.id)
+app.get('/songs/:id', function(req, res) {
     res.sendFile(__dirname + '/public/songs/' + req.params.id);
 });
 
 app.get('/generos', function(req, res) {
     client.connect();
-    client.query('SELECT * from genero', (error, response) => {
+    client.query('SELECT * FROM genero', (error, response) => {
         if(!error) {
             let generos = [];
             response.rows.forEach(element => {
-                generos.push(element)
+                generos.push(element);
             });
             res.json(generos);
         } else {
             res.send("Error fetching genres");
+        }
+    });
+});
+
+app.get('/mostvisited', function(req, res) {
+    client.connect();
+    client.query('SELECT * FROM cancion', (error, response) => {
+        if(!error) {
+            let songs = []
+            response.rows.forEach(element => {
+                songs.push(element);
+            });
+            res.json(songs);
+        } else {
+            res.send("Error fetching most visited");
+        }
+    });
+});
+
+app.get('/songs', function() {
+    client.connect();
+    client.query('SELECT * from cancion', (error, response) => {
+        if(!error) {
+            console.log(response.rows)
+        } else {
+            res.send("Error fetching songs");
         }
     });
 });
