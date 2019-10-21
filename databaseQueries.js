339966f5d7e60a87479557bf93b1fdd6baf04265
ilima <ignacio.lima@anima.edu.uy>
2019-10-21 17:41:48 -0300

    cambios

--- databaseQueries.js
+++ databaseQueries.js
@@ -25,11 +25,11 @@ const getGenres = (request, response) => {
             results.rows.forEach(element => {
                 console.log(element.nombre)
                 /*
-                SELECT * FROM cancion ORDER BY RANDOM() LIMIT 10;
+                    SELECT * FROM cancion ORDER BY RANDOM() LIMIT 10;
 
-SELECT *
-FROM genero INNER JOIN cancion_genero ON genero.nombre = cancion_genero.genero_nombre INNER JOIN cancion ON cancion_genero.id_cancion = cancion.id and cancion_genero.id_usuario = cancion.id_usuario
-WHERE genero.nombre = 'Bachata'
+                    SELECT *
+                    FROM genero INNER JOIN cancion_genero ON genero.nombre = cancion_genero.genero_nombre INNER JOIN cancion ON cancion_genero.id_cancion = cancion.id and cancion_genero.id_usuario = cancion.id_usuario
+                    WHERE genero.nombre = 'Bachata'
                 */
             });
             response.status(200).json(result);
